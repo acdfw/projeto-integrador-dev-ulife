d47127b0f46d99381286abacf1ea2adb80a3e597
Fabio Tomas de Aquino <fabio.aquino@animaeducacao.com.br>
2022-03-28 13:31:48 -0300

    B-02.02: Atualização das entidades e configurando as propriedades que podem ser nulas de acordo com a nova estrutura do .NET 6

--- backend/Anima.ProjetoIntegrador.Domain.Core/Entities/ProvaQuestao.cs
+++ backend/Anima.ProjetoIntegrador.Domain.Core/Entities/ProvaQuestao.cs
@@ -1,12 +1,14 @@
 ﻿using Anima.ProjetoIntegrador.Domain.Shared.Entities;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Anima.ProjetoIntegrador.Domain.Core.Entities
 {
+    [ExcludeFromCodeCoverage]
     public class ProvaQuestao : Entity
     {
-        public Guid ProvaId { get; set; }
-        public virtual Prova Prova { get; set; }
-        public Guid QuestaoId { get; set; }
-        public virtual Questao Questao { get; set; }
+        public Guid? ProvaId { get; set; }
+        public virtual Prova? Prova { get; set; }
+        public Guid? QuestaoId { get; set; }
+        public virtual Questao? Questao { get; set; }
     }
 }
--- backend/Anima.ProjetoIntegrador.Domain.Core/Entities/Questao.cs
+++ backend/Anima.ProjetoIntegrador.Domain.Core/Entities/Questao.cs
@@ -1,13 +1,15 @@
 ﻿using Anima.ProjetoIntegrador.Domain.Shared.Entities;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Anima.ProjetoIntegrador.Domain.Core.Entities
 {
+    [ExcludeFromCodeCoverage]
     public class Questao : Entity
     {
-        public string Enunciado { get; set; }
+        public string? Enunciado { get; set; }
         public Guid ProfessorId { get; set; }
-        public virtual Professor Professor { get; set; }
-        public virtual ICollection<Alternativa> Alternativas { get; set; }
-        public virtual ICollection<ProvaQuestao> ProvasQuestoes { get; set; }
+        public virtual Professor? Professor { get; set; }
+        public virtual ICollection<Alternativa>? Alternativas { get; set; }
+        public virtual ICollection<ProvaQuestao>? ProvasQuestoes { get; set; }
     }
 }
