356a396856d5548777e7f097291521aa88ace009
Deyvid Santana <sdsdeyvidh@outlook.com>
2022-03-28 17:16:27 -0300

    B-02.03 Terminada criação dos outros repositórios.

---
+++ backend/Anima.ProjetoIntegrador.Infrastructure.Data.Persistence/Repositories/AlternativaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Anima.ProjetoIntegrador.Domain.Core.Entities;
+using Anima.ProjetoIntegrador.Domain.Shared.Interfaces;
+using Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Contexts;
+
+namespace Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Repositories
+{
+    public class AlternativaRepository : BaseRepository<Alternativa>, IAlternativaRepository
+    {
+        public AlternativaRepository(IntegradorContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ backend/Anima.ProjetoIntegrador.Infrastructure.Data.Persistence/Repositories/MatriculaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Anima.ProjetoIntegrador.Domain.Core.Entities;
+using Anima.ProjetoIntegrador.Domain.Shared.Interfaces;
+using Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Contexts;
+
+namespace Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Repositories
+{
+    public class MatriculaRepository : BaseRepository<Matricula>, IMatriculaRepository
+    {
+        public MatriculaRepository(IntegradorContext context) : base(context)
+        {
+        }
+    }
+}
