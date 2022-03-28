8ef9e8aeaf8a7210fdf72fc222e23fc75bab8fce
Fabio Tomas de Aquino <fabio.aquino@animaeducacao.com.br>
2022-03-28 13:30:41 -0300

    B-02.02: Mapeamento em FluentAPI das entidades.

---
+++ backend/Anima.ProjetoIntegrador.Infrastructure.Data.Persistence/Configurations/MatriculaConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Anima.ProjetoIntegrador.Domain.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Configurations
+{
+    public class MatriculaConfiguration : IEntityTypeConfiguration<Matricula>
+    {
+        public void Configure(EntityTypeBuilder<Matricula> builder)
+        {
+            builder.ToTable("matricula");
+
+            builder.HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Id)
+                .IsRequired()
+                .HasColumnName("id");
+
+            builder
+                .Property(x => x.AlunoId)
+                .IsRequired()
+                .HasColumnName("aluno_id");
+
+            
+        }
+    }
+}
---
+++ backend/Anima.ProjetoIntegrador.Infrastructure.Data.Persistence/Configurations/ProfessorConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Anima.ProjetoIntegrador.Domain.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Configurations
+{
+    public class ProfessorConfiguration : IEntityTypeConfiguration<Professor>
+    {
+        public void Configure(EntityTypeBuilder<Professor> builder)
+        {
+            builder.ToTable("professor");
+
+            builder.HasKey(x => x.Id);
+
+            builder
+                .Property(x => x.Id)
+                .IsRequired()
+                .HasColumnName("id");
+
+            builder
+                .Property(x => x.UsuarioId)
+                .IsRequired()
+                .HasColumnName("usuario_id");
+
+            
+        }
+    }
+}
