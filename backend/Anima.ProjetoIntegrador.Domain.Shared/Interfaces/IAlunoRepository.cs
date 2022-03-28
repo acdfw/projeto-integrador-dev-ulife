d332a577ac006a5316d85a7ee5ce4be93636f5a3
Deyvid Santana <sdsdeyvidh@outlook.com>
2022-03-28 17:19:26 -0300

    B-02.03 Removidas linhas em branco.

--- backend/Anima.ProjetoIntegrador.Domain.Shared/Interfaces/IAlunoRepository.cs
+++ backend/Anima.ProjetoIntegrador.Domain.Shared/Interfaces/IAlunoRepository.cs
@@ -5,9 +5,7 @@ namespace Anima.ProjetoIntegrador.Domain.Shared.Interfaces
     public interface IAlunoRepository
     {
         IList<AvaliacaoPendenteTurmaAlunoResponse> ConsultarTurmasProfessoresAvaliacoesPendentes(Guid id);
-
         IList<TurmaNaoMatriculadaAlunoResponse> ConsultarTurmasNaoMatriculas(Guid id);
-
         IList<ProvaAbertaPorTurmaAlunoResponse> ConsultarAvaliacoesAbertasDeUmaTurma(Guid id, Guid idTurma);
         IList<ProvaRealizadaPorTurmaAlunoResponse> ConsultarAvaliacoesRealizadasDeUmaTurma(Guid id, Guid idTurma);
         IList<ProvaAbertaTodasTurmasAlunoResponse> ConsultarAvaliacoesAbertasNasTurmaMatriculadas(Guid id);
