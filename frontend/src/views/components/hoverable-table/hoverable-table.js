4f2b709525207902648072d5d079aa0934dfe575
caiohscruz <caiohscruz@hotmail.com>
2022-03-29 15:20:04 -0300

    F-03.02: Adiciona filtro de registros na tabela

--- frontend/src/views/components/hoverable-table/hoverable-table.js
+++ frontend/src/views/components/hoverable-table/hoverable-table.js
@@ -5,28 +5,41 @@ angular.module("website").directive("hoverableTable", function () {
       content: "=",
     },
     templateUrl: "views/components/hoverable-table/hoverable-table.html",
-    controller: function($scope){
-      var me = $scope; 
-      
-     me.resultsPerPage = (!me.content.pagination) ? 5 : me.content.pagination;
+    controller: function ($scope) {
+      var me = $scope;
 
       me.actualPage = 0;
+      
+      me.resultsPerPage = !me.content.pagination ? 5 : me.content.pagination;
 
-      me.nextPage = function(){
-        if(me.resultsPerPage * (me.actualPage + 1 ) < me.content.rows.length){
+      me.nextPage = function () {
+        if (me.resultsPerPage * (me.actualPage + 1) < me.content.rows.length) {
           me.actualPage += 1;
         }
-      }
+      };
 
-      me.previousPage = function(){
-        if(me.resultsPerPage * ( me.actualPage - 1 ) > -1){
+      me.previousPage = function () {
+        if (me.resultsPerPage * (me.actualPage - 1) > -1) {
           me.actualPage -= 1;
         }
-      }
+      };
 
-      me.updateOrdenation =  function(col){
-        me.content.orderBy = col
-      }
-    }
+      me.updateOrdenation = function (col) {
+        me.content.orderBy = col;
+      };
+
+      me.rows = me.content.rows;
+
+      me.filterTable = function (search) {
+        var tempRows = me.content.rows;
+        tempRows = tempRows.filter((row) =>
+          row[me.content.search.sourceColumn]
+            .toUpperCase()
+            .includes(search.toUpperCase())
+        );
+        me.rows = tempRows;
+        me.actualPage = 0;
+      };
+    },
   };
 });
