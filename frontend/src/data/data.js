48b368a167aae7c1e3d1698e99d0686c466688a8
caiohscruz <caiohscruz@hotmail.com>
2022-03-29 15:21:18 -0300

    F-03.01: Atualiza os dados para testes

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,76 +1,72 @@
 var tableClassExams = {
-    rows: [
-      {
-        turma: "Turma A",
-        avaliacao: "A1",
-      },
-      {
-        turma: "Turma A",
-        avaliacao: "A2",
-      },
-      {
-        turma: "Turma B",
-        avaliacao: "A1",
-      },
-      {
-        turma: "Turma B",
-        avaliacao: "A1",
-      },
-    ],
-    colNames: { turma: "Turma", avaliacao: "Avaliação" },
-    colOrder: ["turma", "avaliacao"],
-    showHeader: true,
-  };
-  
-  var tableStudents = {
-    rows: [
-      {
-        matricula: "A102B3",
-        nome: "João",
-      },
-      {
-        matricula: "A10GH4",
-        nome: "Maria",
-      },
-      {
-        matricula: "A234DR",
-        nome: "Pedro",
-      },
-      {
-        matricula: "SDA343",
-        nome: "José",
-      },
-      {
-        matricula: "2133SA",
-        nome: "Augusto",
-      },
-      {
-        matricula: "AS2343",
-        nome: "Fernanda",
-      },
-      {
-        matricula: "FD3423",
-        nome: "Gabriela",
-      },
-    ],
-    colNames: { matricula: "Matrícula", nome: "Nome" },
-    colOrder: ["matricula", "nome"],
-    showHeader: true,
-  };
-  
-  firstColumn = {
-    title: "Avaliações",
-    search: {show: false},
-    table: tableClassExams
-  }
-  
-  secondColumn = {
-    title: "Alunos",
-    search: {show: true, text: "Pesquisar aluno por nome", placeholder: "nome do aluno", sourceColumn: "nome"},
-    table: tableStudents
-  }
+  rows: [
+    {
+      turma: "Turma A",
+      avaliacao: "A1",
+    },
+    {
+      turma: "Turma A",
+      avaliacao: "A2",
+    },
+    {
+      turma: "Turma B",
+      avaliacao: "A1",
+    },
+    {
+      turma: "Turma B",
+      avaliacao: "A1",
+    },
+  ],
+  colNames: { turma: "Turma", avaliacao: "Avaliação" },
+  colOrder: ["turma", "avaliacao"],
+  showHeader: true,
+  search: { show: false },
+};
 
-  classTeacherView = {
-    title: "Turma",
-    columns: [firstColumn, secondColumn]
-  };
+var tableStudents = {
+  rows: [
+    {
+      matricula: "A102B3",
+      nome: "João",
+    },
+    {
+      matricula: "A10GH4",
+      nome: "Maria",
+    },
+    {
+      matricula: "A234DR",
+      nome: "Pedro",
+    },
+    {
+      matricula: "SDA343",
+      nome: "José",
+    },
+    {
+      matricula: "2133SA",
+      nome: "Augusto",
+    },
+    {
+      matricula: "AS2343",
+      nome: "Fernanda",
+    },
+    {
+      matricula: "FD3423",
+      nome: "Gabriela",
+    },
+  ],
+  colNames: { matricula: "Matrícula", nome: "Nome" },
+  colOrder: ["matricula", "nome"],
+  showHeader: true,
+  search: {
+    show: true,
+    text: "Pesquisar aluno por nome",
+    placeholder: "nome do aluno",
+    sourceColumn: "nome",
+  },
+};
+
+classTeacherView = {
+  title: "Turma",
+  subtitles: ["Avaliações", "Alunos"],
+  columns: [tableClassExams, tableStudents],
+};
