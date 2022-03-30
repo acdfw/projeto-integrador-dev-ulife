786adac040d5cc131724c7cc1ddbad2c494add5d
Deyvid Santana <sdsdeyvidh@outlook.com>
2022-03-30 17:21:10 -0300

    B-02.05 Adicionados controllers de aluno e avaliação com endpoints de consultas.

---
+++ backend/Anima.ProjetoIntegrador/Controllers/AlunoController.cs
@@ -0,0 +1,56 @@
+﻿using Anima.ProjetoIntegrador.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anima.ProjetoIntegrador.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlunoController : ControllerBase
+    {
+        private readonly IAlunoService _alunoService;
+
+        public AlunoController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        public IActionResult ConsultarAvaliacoesDeUmaTurmaMatriculada(Guid id, Guid idTurma)
+        {
+            var avaliacoes = _alunoService.ConsultarAvaliacoesDeUmaTurmaMatriculada(id, idTurma);
+
+            if (avaliacoes.Any())
+            {
+                return Ok(avaliacoes);
+            }
+
+            return NotFound("Não existem avaliações pendentes ou realizadas do aluno para a turma");
+        }
+
+        [HttpGet]
+        public IActionResult ConsultarAvaliacoesTurmasMatriculadas(Guid id)
+        {
+            var avaliacoes = _alunoService.ConsultarAvaliacoesTurmasMatriculadas(id);
+
+            if (avaliacoes.Any())
+            {
+                return Ok(avaliacoes);
+            }
+
+            return NotFound("Não existem avaliações pendentes ou realizadas das turmas em que este aluno está matriculado.");
+        }
+
+        [HttpGet]
+        public IActionResult ConsultarTurmasMatriculadasOuNaoDoAluno(Guid id)
+        {
+            var turmas = _alunoService.ConsultarTurmasMatriculadasOuNaoDoAluno(id);
+
+            if (turmas.Any())
+            {
+                return Ok(turmas);
+            }
+
+            return NotFound("Não existem quaisquer turmas no qual o aluno esteja matriculado ou não.");
+        }
+    }
+}
---
+++ backend/Anima.ProjetoIntegrador/Controllers/AvaliacaoController.cs
@@ -0,0 +1,30 @@
+﻿using Anima.ProjetoIntegrador.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anima.ProjetoIntegrador.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AvaliacaoController : ControllerBase
+    {
+        private readonly IAvaliacaoService _avaliacaoService;
+
+        public AvaliacaoController(IAvaliacaoService avaliacaoService)
+        {
+            _avaliacaoService = avaliacaoService;
+        }
+
+        [HttpGet]
+        public IActionResult ObterProvaTurmaQuestoesPorAvaliacao(Guid id)
+        {
+            var provaTurmaComQuestoes = _avaliacaoService.ObterProvaTurmaQuestoesPorAvaliacao(id);
+
+            if (provaTurmaComQuestoes is not null)
+            {
+                return Ok(provaTurmaComQuestoes);
+            }
+
+            return NotFound("Avaliação não encontrada.");
+        }
+    }
+}
