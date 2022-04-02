edf3e5d25e61a536e717e35b2cca11afc9c9b9ce
Deyvid Santana <sdsdeyvidh@outlook.com>
2022-04-02 15:00:25 -0300

    B-02.03 Adicionados repositórios das tabelas intermediárias.

---
+++ backend/Anima.ProjetoIntegrador.Infrastructure.Data.Persistence/Repositories/AvaliacaoMatriculaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Anima.ProjetoIntegrador.Domain.Core.Entities;
+using Anima.ProjetoIntegrador.Domain.Shared.Interfaces;
+using Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Contexts;
+
+namespace Anima.ProjetoIntegrador.Infrastructure.Data.Persistence.Repositories
+{
+    public class AvaliacaoMatriculaRepository : BaseRepository<AvaliacaoMatricula>, IAvaliacaoMatriculaRepository
+    {
+        public AvaliacaoMatriculaRepository(IntegradorContext context) : base(context)
+        {
+        }
+    }
+}
