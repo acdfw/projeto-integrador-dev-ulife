e2e6f2a796408e2a1ea164ed2a9fc5997da85022
caiohscruz <caiohscruz@hotmail.com>
2022-04-05 21:59:39 -0300

    F-06.02: Setup do model para Questões

---
+++ frontend/src/data/questions.data.js
@@ -0,0 +1,219 @@
+questions = [
+  {
+    title: "Proin at neque id mi gravida dignissim?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: " Vivamus libero orci, scelerisque sed bibendum at?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "In eu libero nisi. Vivamus vestibulum velit dictum?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "Proin at neque id mi gravida dignissim?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "nteger ultrices ornare risus, id elementum justo tincidunt eu?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "Proin at neque id mi gravida dignissim?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "Vestibulum varius nec turpis eget bibendum.?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "Aenean accumsan elementum erat, vestibulum fermentum dolor?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "Fusce a odio nulla?",
+    statement:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam odio ac est vestibulum, vel congue metus finibus. Phasellus ultrices risus aliquam leo semper luctus. Ut a molestie ipsum. Duis gravida nisi in purus accumsan convallis. Ut eleifend nisi at massa dignissim euismod. Nullam feugiat, nulla sed elementum faucibus, neque ante maximus dui, id pulvinar elit ligula vitae justo. Donec blandit dolor erat, a porttitor libero dictum ac.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+  {
+    title: "Aenean consequat dapibus dolor vel viverra?",
+    statement:
+      "Donec quis ligula sollicitudin, mollis nulla a, vulputate justo. Nunc sit amet iaculis turpis. Pellentesque tristique magna sit amet tellus viverra euismod. Curabitur odio eros, egestas quis erat nec, faucibus eleifend nulla. Maecenas imperdiet est mollis lacus vehicula, eget tempus felis ultricies. Duis ultrices nisi velit. Sed fermentum sapien tortor, vel semper arcu ultrices nec. Duis et suscipit purus. Sed elementum in erat eget bibendum. Fusce quis felis et mauris porta finibus a vitae ex. Praesent at mollis massa. Sed posuere luctus quam a iaculis. Integer venenatis sapien vitae ante dignissim, sed blandit dui vehicula. Sed commodo dignissim ipsum.",
+    options: [
+      {
+        id: "daskfmkldfklsadlksamd",
+        statement: "Donec quis ligula sollicitudin",
+      },
+      {
+        id: "dasdasdgrefsdfsgdf",
+        statement: "Aenean consequat dapibus dolor vel viverra",
+      },
+      { id: "fsgsfdgterteergdfg", statement: "mollis nulla a, vulputate justo" },
+      {
+        id: "gsgfdhdfgfdtertjhr",
+        statement: "Pellentesque tristique magna sit",
+      },
+      { id: "nfgtrygfdshetrwgdsf", statement: "amet tellus viverra euismod" },
+    ],
+  },
+];
+
+scripts = [
+  "controllers/HomeController.js",
+  "controllers/teacher/TeacherClassController.js",
+  "controllers/teacher/TeacherClassesController.js",
+  "controllers/teacher/TeacherAssignmentsController.js"
+]
