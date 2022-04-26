78d649d6b894668e80cb14218351fb620feeecc7
Deyvid Santana <sdsdeyvidh@outlook.com>
2022-04-26 11:57:14 -0300

    B-02.03, 04 e 05: Iniciados ajustes nos endpoints do aluno

--- backend/Anima.ProjetoIntegrador.Domain.Shared/Responses/ProvaAbertaRealizadaTodasTurmasAlunoResponse.cs
+++ backend/Anima.ProjetoIntegrador.Domain.Shared/Responses/ProvaAbertaRealizadaTodasTurmasAlunoResponse.cs
@@ -3,9 +3,10 @@
     public class ProvaAbertaRealizadaTodasTurmasAlunoResponse : BaseResponse
     {
         public string? IdAvaliacao { get; set; }
-        public string? NomeProva { get; set; }
+        public string? NomeAvaliacao { get; set; }
         public string? NomeTurma { get; set; }
         public string? NomeProfessor { get; set; }
+        public double? Nota { get; set; }
         public bool Realizada { get; set; }
     }
 }
