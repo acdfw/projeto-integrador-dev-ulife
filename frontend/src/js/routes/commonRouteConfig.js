88aec19b78954b74bea5167914710af241560cad
caiohscruz <caiohscruz@hotmail.com>
2022-04-26 17:38:28 -0300

    Inclui tela "Sobre"

--- frontend/src/js/routes/commonRouteConfig.js
+++ frontend/src/js/routes/commonRouteConfig.js
@@ -2,7 +2,8 @@ app.config([
   "$routeProvider",
   function ($routeProvider) {
     $routeProvider
-      .when("/", { templateUrl: "views/home.html", controller: "HomeCtrl" })      
+      .when("/", { templateUrl: "views/home.html", controller: "HomeCtrl" })  
+      .when("/about", { templateUrl: "views/about.html", controller: "AboutCtrl" })     
       .when("/logout", {
         resolve: {
           logout: [
